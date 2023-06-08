e5ba8509526aee0bd2d66eee16a16b5cc11f81c6
Alonso Chavarria <alons182@hotmail.com>
2023-06-08 08:39:11 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <div class="container mx-auto px-4 py-6">
+    <RouterView />
+  </div>
+</template>
+
